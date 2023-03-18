a5f26829c45584f86c0df30ca1c6e4e279cc3f06
fadhillahramadhan <fadhillahramadhan01@gmail.com>
2023-03-18 14:08:50 +0700

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+// FAMILY SEEDER
+use Database\Seeders\FamilySeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(FamilySeeder::class);
+
         // \App\Models\User::factory(10)->create();
     }
 }
